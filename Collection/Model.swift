10fac5ff306ce7bfb4bf76393b294054326dc84c
DimaParachute <dimaparachute11@icloud.com>
2023-06-30 19:41:06 +0300

    Исправлены ошибки в модельке, сетевой загрузке, настройке ячейки

--- Collection/Model.swift
+++ Collection/Model.swift
@@ -8,31 +8,9 @@
 import Foundation
 
 struct UnsplashPhoto: Codable {
-    let id: String
-    let createdAt: String
-    let width: Int
-    let height: Int
-    let color: String
     let urls: URLs
-    let user: User
 
     struct URLs: Codable {
-        let raw: String
-        let full: String
         let regular: String
-        let small: String
-        let thumb: String
-    }
-
-    struct User: Codable {
-        let name: String
-        let username: String
-        let profileImage: ProfileImage
-
-        struct ProfileImage: Codable {
-            let small: String
-            let medium: String
-            let large: String
-        }
     }
 }
